9ac42c3685fb545599f21b20b291badff3ce298f
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-09-23 03:41:23 -0400

    Update shift

--- src/shift.jl
+++ src/shift.jl
@@ -3,11 +3,16 @@ export Shift
 import IterativeSolvers
 import LinearAlgebra
 
-struct Shift{S<:Number, LinOp<:AbstractMatrix} <: AbstractMatrix{S}
+struct Shift{S<:Number, L<:AbstractMatrix}<:AbstractMatrix{S}
     shift::S
-    operation::LinOp
+    operation::L
+
     function Shift(shift::S, operation::L) where {S<:Number, L<:AbstractMatrix}
-        S2 = promote_type(S, valtype(operation))
+        S2 = promote_type(S, eltype(operation))
+        new{S2, L}(convert(S2, shift), operation)
+    end
+
+    function Shift{S2}(shift::S, operation::L) where {S<:Number, L<:AbstractMatrix, S2}
         new{S2, L}(convert(S2, shift), operation)
     end
 end
@@ -17,14 +22,21 @@ function Base.:(*)(m::Shift, x::AbstractVector)
 end
 
 function LinearAlgebra.mul!(y::AbstractVector, m::Shift, x::AbstractVector)
-    mul!(y, m.operation, x)
-    y .+= m.shift .* x
+    LinearAlgebra.mul!(y, m.operation, x)
+    LinearAlgebra.BLAS.axpy!(m.shift, x, y)
     return y
 end
 
-Base.size(s::Shift) = size(s.operation)
+Base.size(s::Shift, args...) = size(s.operation, args...)
 
-function Base.show(context::IOContext, mime::MIME, m::Shift)
-    Base.show(context, mime, "Shift ($(m.shift)) of ")
-    Base.show(context, mime, m.operation)
+function Base.show(io::IO, mime::MIME, m::Shift)
+    Base.show(io, mime, "Shift ($(m.shift)) of ")
+    Base.show(io, mime, m.operation)
 end
+
+function Base.display(s::Shift)
+    println("Shift ($(s.shift)) of: ")
+    display(s.operation)
+end
+
+# TODO: shift composition?
