d54f7d2722ed808f27d5359a35bf95af26301e16
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2021-01-22 23:36:04 -0500

    Add display for LinearAlgebra

--- src/linearoperator.jl
+++ src/linearoperator.jl
@@ -10,6 +10,14 @@ struct LinearOperator{
 }<:AbstractMatrix{S}
     size::Tuple{Int, Int}
     operation!::A
+
+    @doc """
+        LinearOperator(type, size, operation!)
+    
+    Create a linear operator (matrix) of given size.
+
+    operation! is a function `operation!(y, x)` is called by `mul!(y, op, x)`
+    """
     function LinearOperator(::Type{S}, size::Tuple{<:Integer, <:Integer}, operation!::A) where {S<:Number, A<:Function}
         return new{S, A}(size, operation!)
     end
@@ -26,4 +34,8 @@ Base.eltype(::Type{LinearOperator{S, A}}) where {S, A} = S
 
 function Base.show(io::IO, mime::MIME, m::LinearOperator)
     Base.show(io, mime, "LinearOperator with $(m.operation!)")
+end
+
+function Base.display(s::LinearOperator)
+    println("LinearOperator of size $(size) and operation $(s.operation!)")
 end
